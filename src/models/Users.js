f51992b2398be15d1eac8ce1d8ede7f32b7e16fb
celiogouvea <celiogomesgouvea@gmail.com>
2019-09-26 15:44:28 -0300

    auteração dados users

--- src/models/Users.js
+++ src/models/Users.js
@@ -2,43 +2,73 @@ const mongoose = require('mongoose');
 
 const UsersSchema = new mongoose.Schema({
 
-   nome:{
-     type: String,
-     require: true,
-   },
+  nome: {
+    type: String,
+    require: true,
+  },
 
-   cpf:{
-     type: String,
-     require: true,
-   },
+  cpf: {
+    type: String,
+    require: true,
+  },
 
-   dataNascimento:{
-     type: String,
-     require: true,
-   },
+  dataNascimento: {
+    type: String,
+    require: true,
+  },
 
-   telefone:{
-     type: String,
-     require: true,
-   },
+  telefone: {
+    type: String,
+    require: true,
+  },
 
-   setor:{
-     type: String,
-     require: true,
-   },
+  setor: {
+    type: String,
+    require: true,
+  },
 
-   endereco:{
-     type: String,
-     require: true,
-   },
+  endereco: {
+    type: Array,
+    require: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true,
+  },
+  cpf: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  passwordResetToken: {
+    type: String,
+    select: false,
+  },
+  passwordResetExpires: {
+    type: Date,
+    select: false,
+  },
 
-   createAt:{
-     type: Date,
-     default: Date.now,
-   },
+  id_setor:{
+    type:String,
+    require: true,
+  },
+
+  createAt: {
+    type: Date,
+    default: Date.now,
+  },
 
 
 });
 
 
-mongoose.model('User',UsersSchema);
+mongoose.model('User', UsersSchema);
